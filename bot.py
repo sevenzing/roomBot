eb20752da0fd964172490a00da619d237e5a217c
sevenzing <puka0077@mail.ru>
2019-11-28 23:56:01 +0300

    asd

--- bot.py
+++ bot.py
@@ -1,12 +1,31 @@
 import telebot
-import src
+import os
+from flask import Flask, request
+import logging
 
-TOKEN = '982620066:AAESmASPIDYIM7bGOyCCl6r8cmO7r5qTuSo'
-bot = telebot.TeleBot(TOKEN)
+token = '982620066:AAESmASPIDYIM7bGOyCCl6r8cmO7r5qTuSo'
+bot = telebot.TeleBot(token)
 
+# Здесь пишем наши хэндлеры
 
-@bot.message_handler(content_types=['text'])
-lambda x: src.start(x, bot)
+# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
 
-
-bot.polling()
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://tgbot1234.herokuapp.com/") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
