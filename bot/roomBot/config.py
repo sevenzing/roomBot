7e69285a7d676e17426e778a29e40f5250b8ce8d
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-03-05 14:31:24 +0300

    fixed bug with read timeout

--- bot/roomBot/config.py
+++ bot/roomBot/config.py
@@ -3,26 +3,22 @@ import configparser
 PATH_TO_BOT_LOG_FILE = 'roomBot.log'
 PATH_TO_NOTIFICATION_LOG_FILE = 'notification.log'
 
-# --------
+# ========
 # Messages
-# --------
+# ========
 
-START_MESSAGE = "Hi! I'm a room bot. I can do some useful things. \n\n"\
+START_MESSAGE = "Hi! I'm room bot. \n\n"\
                 "📅 First, select your building to understand the cleaning schedule\n"\
-                "😎 If we are in a chat add all your roommates /addmembers\n"\
+                "😎 You can /add item to your buylist and manage it using /list\n"\
                 "❓ When the next cleaning day? /nextcleaning\n"\
-                "⏰ The bot sends a notification on every cleaning day at 8am \n"\
                 "⏩ Did you make a mistake? Change your building /changebuilding \n"
+                "⏰ The bot sends a notification on every cleaning day at 8am \n"\
 
 HELP_MESSAGE = """help message"""
 
 CHANGE_BUILDING_MESSAGE = "🏣 Please, select your building\n"
 
 NOTICE_MESSAGE_START = "Hello! Don't forget about cleaning today!"
-
-ADDMEMBER_ERORR = "Specify aliases after the command. \nFor example: /addmember @me @myfriend @myfriend2"
-
-SUCCESS_ADDMEMBER = "Great! Now you have \n%s \nin your notice list"
 
 NEXT_DAY = "Your building is %s, so the next cleaning day will be in *%d days*, on the *%s of %s*.\n"
 
@@ -34,19 +30,21 @@ HAVE_NOT_BUILDING = "You haven't chosen a building yet. \n/changebuilding"
 #  Buy list 
 # ==========
 
-SEND_BUY_LIST = "your list:"
+SEND_BUY_LIST = "Your list:"
 
-CHANGE_KEYBOARD_DECREASE_TEXT = "[decrease]"
+CHANGE_KEYBOARD_DECREASE_TEXT = "/Decrease/"
 
-CHANGE_KEYBOARD_CLOSE_TEXT = "[close]"
+CHANGE_KEYBOARD_CLOSE_TEXT = "/Close/"
 
-CHANGE_KEYBOARD_CLEAR_TEXT = "[clear]"
+CHANGE_KEYBOARD_CLEAR_TEXT = "/Clear/"
 
 ADD_ITEM_TO_LIST = "Specify the items you want to buy like this:*\nitem1\nitem number 2\nitem #3*"
 
 LIST_WAS_DELETED = "The list was cleared"
 
 ITEMS_ADDED = "Acknowledged, %s"
+
+# -- | Callback data
 
 INCREASE = 'increase'
 
--- bot/run.py
+++ bot/run.py
@@ -1,8 +1,15 @@
+import requests
+
 from roomBot.bot import start
 from roomBot import tools
 
 try:
     start()
+
+except requests.exceptions.ReadTimeout as e:
+    tools.log(e, error=True)
+    start()
+    
 except Exception as e:
     tools.log(e, error=True)
     raise e
