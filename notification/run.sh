61cbe4548a6b97fd5f6a9751839324a9c7d0808b
Lev Lymarenko <sevenzing@gitlab.com>
2020-05-11 11:48:27 +0300

    forever loop for notification

---
+++ notification/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo_and_log()
+{
+  message=$1
+  # to console
+  echo $message 
+  # to log file
+  echo "$(date '+%Y-%m-%d %H:%M:%S') | run.sh script |" $message >> notification.log
+  # to telegram
+  python run.py --send-message "${message}"
+}
+
+
+echo_and_log "Starting notification"
+python -u run.py --start
+result=$?
+
+time_start=$(($(date +%s%N)/1000000000)) # IN SECONDS 
+flag=0
+
+# While true
+while [ ${result} != -1 ]; do
+    echo_and_log "Got exit code ${result}. Restarting bot in 10 seconds"
+    sleep 10
+    echo_and_log "Starting bot"
+    python -u run.py --start
+    result=$?
+    time_finished=$(($(date +%s%N)/1000000000)) # IN SECONDS 
+
+    amount_to_sleep=`expr 120 - ${time_finished} + ${time_start}`
+    
+    amount_to_sleep=$((amount_to_sleep>0 ? amount_to_sleep : 5))
+
+    echo_and_log "Telegram error 137: Sleep in ${amount_to_sleep} seconds"
+
+    sleep ${amount_to_sleep}
+
+    
+    time_start=${time_finished}
+done
+
+
+echo_and_log "Bot exited with code ${result}. ALERT! bot stopped."
+exit $result
